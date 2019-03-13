5ff598b9b5483c28c841366126c206f46d79bcff
monjudoh <monjudoh@gmail.com>
2019-03-13 12:11:40 +0900

    navigator.pointerEnabledは削除されているので判定には使えない

--- src/BrowserEventTranslator/env/supports.js
+++ src/BrowserEventTranslator/env/supports.js
@@ -1,6 +1,6 @@
 define('BrowserEventTranslator/env/supports',[],function(){
   const supports = Object.create(null);
-  supports.PointerEvent = !!navigator.pointerEnabled;
+  supports.PointerEvent = 'onpointerdown' in document.documentElement;
   supports.TouchEvent = 'ontouchstart' in document.documentElement;
   supports.MouseEvent = 'onmousedown' in document.documentElement;
   Object.freeze(supports);
